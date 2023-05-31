cdde4c9b1d3eda82073bacff8fb87ed513e5ff2f
Eray Demircioğlu <c.eray@hotmail.com>
2023-05-31 19:03:24 +0300

    Youtube

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Header from "./components/Header";
+import Feed from "./components/Feed";
+import SearchResult from "./components/SearchResult";
+import VideoDetails from "./components/VideoDetails";
+import { AppContext } from "./context/contextApi";
+
+const App = () => {
+    return (
+        <AppContext>
+            <BrowserRouter>
+                <div className="flex flex-col h-full">
+                    <Header />
+                    <Routes>
+                        <Route path="/" exact element={<Feed />} />
+                        <Route
+                            path="/searchResult/:searchQuery"
+                            element={<SearchResult />}
+                        />
+                        <Route path="/video/:id" element={<VideoDetails />} />
+                    </Routes>
+                </div>
+            </BrowserRouter>
+        </AppContext>
+    );
+};
 
 export default App;
